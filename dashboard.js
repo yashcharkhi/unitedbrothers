6f3ac70a59853a32800d659f8e7d2b2ba953f649
yashcharkhi <68316385+yashcharkhi@users.noreply.github.com>
2023-12-07 14:16:44 +0530

    file upload

---
+++ dashboard.js
@@ -0,0 +1,41 @@
+/* globals Chart:false */
+
+(() => {
+  'use strict'
+
+  // Graphs
+  const ctx = document.getElementById('myChart')
+  // eslint-disable-next-line no-unused-vars
+  const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: [
+        'Nov',
+        'Dec',
+        'Jan',
+        'Feb'
+      ],
+      datasets: [{
+        data: [
+          500,
+          2100
+        ],
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#007bff',
+        borderWidth: 4,
+        pointBackgroundColor: '#007bff'
+      }]
+    },
+    options: {
+      plugins: {
+        legend: {
+          display: false
+        },
+        tooltip: {
+          boxPadding: 3
+        }
+      }
+    }
+  })
+})()
